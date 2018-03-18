5fd0a4f4f8bf7ecae2533974fbc40d26091096f5
Steffen Dam <sdam@incom-it.dk>
2018-03-18 12:07:32 +0100

    Minor refactoring

--- B2B/Buisness.cs
+++ B2B/Buisness.cs
@@ -20,12 +20,9 @@ namespace B2B
                                            ProcessRequest);
                 _bus.Subscribe<OrderReply>("DepartmentReplies",
                                            ProcessReply, x => x.WithQueueName(nameof(Order)));
-
-                //bus.Subscribe<OrderReply>("DepartmentBroadcastReplies",
-                //                           ProcessBroadCastReply, x => x.WithQueueName(nameof(BroadCastOrder)));
                 _bus.Subscribe<OrderReply>("DepartmentBroadcastReplies",
                                            ProcessBroadCastReply, x => x.WithQueueName(nameof(BroadCastOrder)));
-                
+
                 Console.WriteLine("Subscribed");
                 lock (this)
                 {
@@ -45,78 +42,86 @@ namespace B2B
             Console.WriteLine("Published Request");
         }
 
-           private void ProcessBroadCastReply(OrderReply message)
+        private void ProcessBroadCastReply(OrderReply message)
         {
-                Console.WriteLine("Proccesing Broadcast reply");
-                if(!_broadcastOrders.Any(x => x.Key == message.OrderId))
+            Console.WriteLine("Proccesing Broadcast reply");
+            if (_broadcastOrders.All(x => x.Key != message.OrderId))
             {
-                _broadcastOrders.Add(message.OrderId, new List<OrderReply>() { message });
+                _broadcastOrders.Add(message.OrderId, new List<OrderReply> { message });
 
             }
             var dictItem = _broadcastOrders[message.OrderId];
-            if (dictItem != null && !dictItem.Contains(message))
-            {
-
-                dictItem.Add(message);
-               
-            }
-           
-            var order = _outstandingOrders.FirstOrDefault(x => x.Id == message.OrderId);
-            if(order != null) { 
-            if (dictItem.Any(x => x.DeliveryType == DeliveryEnum.Full))
-            {
-                Console.WriteLine("Full order delivery possible");
-                _bus.Publish(dictItem.FirstOrDefault(x => x.DeliveryType == DeliveryEnum.Full), order.Customer.Id);
-                    _outstandingOrders.Remove(order);
+            if (dictItem == null)
                 return;
-            }else if (dictItem.Where(x => x.Product != null).Sum(x => x.Product.Amount) > order.Product.Amount)
-            {
-                Console.WriteLine("Multiple partial delivery possible");
 
-                var currentOrders = new List<OrderReply>();
-                while(currentOrders.Sum(x => x.Product.Amount) < order.Product.Amount){
-                    var maxStock = dictItem.Max(x => x.Product.Amount);
-                    currentOrders.AddRange(dictItem.Where(x => x.Product.Amount == maxStock));
-                }
-                var replyMessage = new OrderReply(-2, order.Id, order.Product, DeliveryEnum.Full, null)
+            if (!dictItem.Contains(message))
+            {
+                dictItem.Add(message);
+            }
+
+            var order = _outstandingOrders.FirstOrDefault(x => x.Id == message.OrderId);
+            if (order != null)
+            {
+                if (dictItem.Any(x => x.DeliveryType == DeliveryEnum.Full))
                 {
-                    DeliveryTime = currentOrders.Max(x => x.DeliveryTime),
-                    ShippingFee = currentOrders.Sum(x => x.ShippingFee)
-                };
-                _bus.Publish(replyMessage, order.Customer.Id);
+                    Console.WriteLine("Full order delivery possible");
+                    _bus.Publish(dictItem.FirstOrDefault(x => x.DeliveryType == DeliveryEnum.Full), order.Customer.Id);
+                    _outstandingOrders.Remove(order);
+                    return;
+                }
+                if (dictItem.Where(x => x.Product != null).Sum(x => x.Product.Amount) > order.Product.Amount)
+                {
+                    Console.WriteLine("Multiple partial delivery possible");
+
+                    var currentOrders = new List<OrderReply>();
+                    while (currentOrders.Sum(x => x.Product.Amount) < order.Product.Amount)
+                    {
+                        int maxStock = dictItem.Max(x => x.Product.Amount);
+                        IEnumerable<OrderReply> items = dictItem.Where(x => x.Product.Amount == maxStock).ToArray();
+                        currentOrders.AddRange(items);
+                        foreach (OrderReply item in items)
+                        {
+                            dictItem.Remove(item);
+                        }
+                    }
+                    var replyMessage = new OrderReply(-2, order.Id, order.Product, DeliveryEnum.Full, null)
+                    {
+                        DeliveryTime = currentOrders.Max(x => x.DeliveryTime),
+                        ShippingFee = currentOrders.Sum(x => x.ShippingFee)
+                    };
+                    _bus.Publish(replyMessage, order.Customer.Id);
                     _outstandingOrders.Remove(order);
                     return;
 
-            }
-            else if (dictItem.Count == 3)
-            {
-                Console.WriteLine("No delivery possible");
-                _bus.Publish(new OrderReply(-1, message.OrderId, order.Product, DeliveryEnum.None, null), order.Customer.Id);
+                }
+                if (dictItem.Count == 3)
+                {
+                    Console.WriteLine("No delivery possible");
+                    _bus.Publish(new OrderReply(-1, message.OrderId, order.Product, DeliveryEnum.None, null), order.Customer.Id);
                     _outstandingOrders.Remove(order);
                 }
             }
         }
-   
+
         private void ProcessReply(OrderReply message)
         {
-                Console.WriteLine("Proccesing reply");
-                var order = _outstandingOrders.FirstOrDefault(x => x.Id == message.OrderId);
-            if (order != null)
+            Console.WriteLine("Proccesing reply");
+            var order = _outstandingOrders.FirstOrDefault(x => x.Id == message.OrderId);
+            if (order == null)
+                return;
+            if (message.DeliveryType == DeliveryEnum.Full)
             {
-                if (message.DeliveryType == DeliveryEnum.Full)
+                _bus.Publish(message, order.Customer.Id);
+                Console.WriteLine("Sent reply");
+                _outstandingOrders.Remove(order);
+            }
+            else
+            {
+                _bus.Publish(new BroadCastOrder(order.Id, order.Product, order.Customer)
                 {
-                    _bus.Publish(message, order.Customer.Id);
-                    Console.WriteLine("Sent reply");
-                    _outstandingOrders.Remove(order);
-                }
-                else
-                {
-                    _bus.Publish(new BroadCastOrder(order.Id, order.Product, order.Customer)
-                    {
-                        ReplyTo = nameof(BroadCastOrder)
-                    });
-                    Console.WriteLine("Broadcasted request");
-                }
+                    ReplyTo = nameof(BroadCastOrder)
+                });
+                Console.WriteLine("Broadcasted request");
             }
         }
     }
